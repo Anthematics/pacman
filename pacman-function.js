73428d7c770df64ff3af183d5639adc6a62bad76
Anthematics <jasonharder2@gmail.com>
2017-09-25 16:06:42 -0400

    pacman can now eat dots.

---
+++ pacman-function.js
@@ -0,0 +1,17 @@
+//There are 2 main ways to define functions in JS
+
+//Named function
+
+function eatDot() {
+	console.log("MUNCH!");
+}
+
+//Anonymous function - where we assign the function to a variable instead of giving it a direct name.
+
+var eatDot = function() {
+	console.log("MUNCH!");
+}
+
+//Call it with
+
+	eatDot();
